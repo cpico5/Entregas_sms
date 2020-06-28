696a92471eff665f9272b65775584c94f8373ed0
cpico5 <cpico56@gmail.com>
2020-06-28 14:12:01 -0500

    2

---
+++ app/src/main/java/mx/gob/cdmx/entregassms/Nombre.java
@@ -0,0 +1,25 @@
+package mx.gob.cdmx.entregassms;
+
+public class Nombre  {
+
+    public static final String customURL = "https://opinion.cdmx.gob.mx/encuestas/";
+    public static final String encuesta = "entrega_sms";
+    public static final String USUARIO = "usuario";
+    public static final String PADRON = "padron";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String IMEI = "imei";
+
+    public String nombreEncuesta(){
+
+        final String nombreEncuesta = "entrega_sms";
+        return nombreEncuesta;
+    }
+
+    public String nombreDatos(){
+
+        final String nombreEncuesta = "datos_entrega_sms";
+        return nombreEncuesta;
+    }
+
+}	 
